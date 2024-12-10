91d98792c198e21daf8d75086d8391197514169c
manisha4450 <chowdaryc226@gmail.com>
2024-12-10 10:31:11 +0530

    Add files via upload

---
+++ ACplease.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Acplease
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	Scanner sc=new Scanner(System.in);
+	int t=sc.nextInt();
+	if(t>30)
+	System.out.println("yes");
+	else
+	System.out.println("no");
+	}
+}
