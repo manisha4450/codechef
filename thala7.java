ff6924672af52936d328db1539b1c0b8a95cfb96
manisha4450 <chowdaryc226@gmail.com>
2024-12-10 11:55:58 +0530

    Add files via upload

---
+++ thala7.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class thala7
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int N=sc.nextInt();
+		if(N==7)
+		{
+		    System.out.println("THALA");
+		    
+		}
+		    else{
+		        System.out.println("SADGE");
+		    }
+		}
+	}
