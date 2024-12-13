2ace6f0e613046a59842d9e46c6b0abc9e7ef619
manisha4450 <chowdaryc226@gmail.com>
2024-12-13 15:49:47 +0530

    Add files via upload

---
+++ icecone.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class icecone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner sc=new Scanner(System.in);
+	    int a=sc.nextInt();
+	    int b=sc.nextInt();
+	    int result = Math.min(a,b);
+	    System.out.println(result);
+
+
+	}
+}
