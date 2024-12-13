d0f03a48930eff7bddf6aae1f238a06b6231ec23
manisha4450 <chowdaryc226@gmail.com>
2024-12-13 15:35:54 +0530

    Add files via upload

---
+++ ccov.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class ccov
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+	int x=sc.nextInt();
+	if(x>40)
+	System.out.println("yes");
+	else
+	System.out.println("no");
+
+	}
+}
