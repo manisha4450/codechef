e14b591b5bd3d0c100b87e2de15f79b694b2a93b
manisha4450 <chowdaryc226@gmail.com>
2024-12-13 15:53:46 +0530

    Add files via upload

---
+++ glpr.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class glpr
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int X=sc.nextInt();
+		int Y=sc.nextInt();
+		if(2*X>=Y)
+		{
+		    System.out.println("METAL");}
+		    else{
+		        System.out.println("PLASTIC");
+		    }
+		}
+
+	}
+
