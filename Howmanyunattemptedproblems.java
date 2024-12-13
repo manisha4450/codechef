cc0adca535fddee465ddfa8ecead87522cc062a2
manisha4450 <chowdaryc226@gmail.com>
2024-12-13 15:31:03 +0530

    Add files via upload

---
+++ Howmanyunattemptedproblems.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Howmanyunattemptedproblems
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int x=sc.nextInt();
+		int y=sc.nextInt();
+		System.out.println(x-y);
+
+	}
+}
