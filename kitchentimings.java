09b0416978bb37818b31b92cdaafeee400f77523
manisha4450 <chowdaryc226@gmail.com>
2024-12-13 15:32:47 +0530

    Add files via upload

---
+++ kitchentimings.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class kitchentimings
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	Scanner sc=new Scanner(System.in);
+	int t=sc.nextInt();
+	for(int i=0;i<t;i++)
+	{
+	    int x=sc.nextInt();
+	    int y=sc.nextInt();
+	    System.out.println(y-x);
+	}
+	}
+}
