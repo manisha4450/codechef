fe2a05fa3799c638730eae4d7bbd67e2d348aa0b
manisha4450 <chowdaryc226@gmail.com>
2024-12-13 15:55:19 +0530

    Add files via upload

---
+++ luclo.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class luclo
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	Scanner sc=new Scanner(System.in);
+	int a=sc.nextInt();
+	int d=((a-1)*3+4);
+	System.out.println(d);
+	}
+}
