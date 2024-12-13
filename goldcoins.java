5c6487febeabf26c01ff75fa54ff98db8f7db0fb
manisha4450 <chowdaryc226@gmail.com>
2024-12-13 15:42:33 +0530

    Add files via upload

---
+++ goldcoins.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class goldcoins
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc= new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int x = sc.nextInt();
+		int y = sc.nextInt();
+		if(x>y)
+		System.out.println(a);
+		else 
+		System.out.println(b);
+        
+
+
+	}
+}
