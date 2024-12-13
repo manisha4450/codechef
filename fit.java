5787edc66f8aa4c3a8640104ad79e4f4fa751dd0
manisha4450 <chowdaryc226@gmail.com>
2024-12-13 15:40:49 +0530

    Add files via upload

---
+++ fit.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class fit{
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+            int T = sc.nextInt();
+		for (int i=0;i<T;i++)
+		{
+		    int X= sc.nextInt();
+		    System.out.println((X*2)*5);
+		}
+            
+        }
+    }
