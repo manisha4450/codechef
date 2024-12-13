f66cd72234d9eb3ec0d47adde4321c2402991d91
manisha4450 <chowdaryc226@gmail.com>
2024-12-13 15:56:51 +0530

    Add files via upload

---
+++ foodcost.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class foodcost
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	Scanner sc=new Scanner(System.in);
+	int x=sc.nextInt();
+	int y=sc.nextInt();
+	System.out.println((6*x)+y);
+
+	}
+}
