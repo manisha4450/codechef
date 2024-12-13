be838c220188e0c0cb1b1033c54ed31b9da4cd8d
manisha4450 <chowdaryc226@gmail.com>
2024-12-13 15:46:31 +0530

    Add files via upload

---
+++ minwage.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class minwage
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	Scanner scn = new Scanner(System.in);
+		int x = scn.nextInt();
+		if(x>11)
+		System.out.println("yes");
+		else
+		System.out.println("no");
+	}
+}
