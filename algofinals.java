8f60ac72662c69b681bb6225bd3f31e5be7208e7
manisha4450 <chowdaryc226@gmail.com>
2024-12-13 15:37:52 +0530

    Add files via upload

---
+++ algofinals.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class algofinals
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	Scanner sc=new Scanner(System.in);
+	int x=sc.nextInt();
+	if(x==17)
+	System.out.println("yay");
+	else
+	System.out.println("no");
+	}
+}
