b030f5128b6eee7dcff883225540f0429a84d4eb
manisha4450 <chowdaryc226@gmail.com>
2024-12-13 15:23:56 +0530

    Add files via upload

---
+++ JustOneMoreEpisode.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class JustOneMoreEpisode
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	Scanner sc=new Scanner(System.in);
+	int t=sc.nextInt();
+	while(t-->0)
+	{
+	    int x=sc.nextInt();
+	    if(x>24)
+	    System.out.println("yes");
+	    else
+	    System.out.println("no");
+	}
+
+	}
+}
