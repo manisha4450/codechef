763353652f1c89ca08ecd5918b7433d4efcae038
manisha4450 <chowdaryc226@gmail.com>
2024-12-13 15:39:29 +0530

    Add files via upload

---
+++ sweetshop.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class sweetshop
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	 Scanner sc=new Scanner(System.in);
+	 int x=sc.nextInt();
+	 int n=sc.nextInt();
+	 int remaings=x-(n*10);
+	 System.out.println(remaings/20);
+	}
+}
