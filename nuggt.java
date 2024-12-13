4c38985b473c7476ba849186422858bbfb31f513
manisha4450 <chowdaryc226@gmail.com>
2024-12-13 15:48:03 +0530

    Add files via upload

---
+++ nuggt.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class nuggt
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	Scanner sc = new Scanner(System.in);
+		int x = sc.nextInt();
+		int y = sc.nextInt();
+		System.out.print(5000*x + 9800*y);
+
+}
+}
