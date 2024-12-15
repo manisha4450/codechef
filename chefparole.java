71b4ba4359190057e541d82e00dac3f9df288c15
manisha4450 <chowdaryc226@gmail.com>
2024-12-15 11:32:00 +0530

    Add files via upload

---
+++ chefparole.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class chefparole
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	Scanner sc=new Scanner(System.in);
+	int X=sc.nextInt();
+	if(X>=7)
+	{
+	    System.out.println("Yes");
+	}else{
+	    System.out.println("No");
+	}
+
+	}
+}
